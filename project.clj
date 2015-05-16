a3765efbd47fd4739b3afc905233ced9233de908
Thorsten Blum <thorsten.blum@toblux.com>
2015-05-16 02:52:45 +0200

    Up lein-cljsbuild

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-3211"]]
 
-  :plugins [[lein-cljsbuild "1.0.5"]]
+  :plugins [[lein-cljsbuild "1.0.6"]]
 
   :clean-targets ^{:protect false} ["target" "resources/public/js"]
 
